af058a48a644608eb4f1ffdda4cfc65a2337f906
praylee0226 <praylee917537@gmail.com>
2023-10-31 02:32:58 +0900

    [Feat] Change Script Name  and Prefab
    Combo.cs -> ComboManager.cs
    and Prefab ComboManager.cs

---
+++ NeonSpace/Assets/Scripts/Manager/ComboManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour
+{
+    public static ComboManager instance;
+    private int comboCnt ;
+   
+
+    UILabel comboLabel; 
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        comboCnt = 0;
+    }
+    public void CountCombo()
+    {
+            comboCnt ++ ;
+            Debug.Log(comboCnt);
+    }
+
+    public void ShowComboCount()
+    {
+            comboLabel.text = comboCnt.ToString();
+    }
+   
+}
