b8559a2371f82b73f1036eb715aa6c379b0c06a3
3tPepper <3tpepperok@gmail.com>
2023-10-31 15:47:19 +0900

    [Feat] 획득 점수에 따라 추가적인 코인 획득&UI 구현 완료

--- NeonSpace/Assets/Scripts/Contents/SideWall.cs
+++ NeonSpace/Assets/Scripts/Contents/SideWall.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class SideWall : MonoBehaviour
 {
+    //게임 화면 하단에 위치한 콜라이더 오브젝트
+
+    //해당 오브젝트의 Trigger 처리
     void OnTriggerEnter2D(Collider2D cd)
     {
+        Debug.Log("트리거");
+        //Trigger된 것이 벽돌일 경우,
         if (cd.gameObject.CompareTag("Brick"))
         {
+
+            //moreCnt_가 1보다 작다면 == 블록이 더 내려갈 줄이 없다면
             if (BrickManager.instance.moreCnt_ < 1)
             {
                 UIManager.instance.Continue(true);
@@ -19,18 +26,23 @@ public class SideWall : MonoBehaviour
                 UIManager.instance.ResultCharSet();
             }
             SoundManager.instance.ChangeEffects(5);
+            //광고 시청하고 기회 얻기를 위한 처리
             BrickManager.instance.lastMoreCheck = cd.GetComponentInParent<BrickCount>();
         }
 
+        //Trigger된 것이 아이템일 경우
         if (cd.gameObject.CompareTag("Item"))
         {
             Items item_ = cd.gameObject.GetComponent<Items>();
             if (item_ == null) return;
+
+            //아이템 정보가 ball이면(공 추가 아이템), 소지 공 카운트를 + 1 하고 비활성화
             if (item_.itemType == Items.ItemInfo.Ball)
             {
                 BallManager.instance.PlusBall(1);
                 item_.BottomTrue();
             }
+            //아니면 그냥 비활성화
             else
             {
                 item_.BottomTrue();
--- NeonSpace/Assets/Scripts/Manager/TweenbundleManager.cs
+++ NeonSpace/Assets/Scripts/Manager/TweenbundleManager.cs
@@ -6,6 +6,7 @@ public class TweenbundleManager : MonoBehaviour
 {
     [SerializeField] LobbyTween[] bundleObj;
 
+    //해당하는 오브젝트(화면) 활성/비활성화 처리
     public void ShowBundleObject(EnumBase.UIState state_ , bool isTrue)
     {
         for(int i = 0; i < bundleObj.Length; ++i)
