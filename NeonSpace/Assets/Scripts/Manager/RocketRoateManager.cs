dc39d8a88675ad0701b442059bac2a7b6301b234
praylee0226 <praylee917537@gmail.com>
2023-11-03 11:51:31 +0900

    [Update] Upgrade RotateRocket

--- NeonSpace/Assets/Scripts/Manager/RocketRoateManager.cs
+++ NeonSpace/Assets/Scripts/Manager/RocketRoateManager.cs
@@ -26,17 +26,18 @@ public class RocketRoateManager : MonoBehaviour
         Vector3 mousePosition = UICamera.lastEventPosition ;
         // 원래는 m_transform.position으로 값 받아왔지만
         // 마우스가 탐지하는 좌표랑 해당 좌표의 괴리값이 있어서 그냥 고정값으로 넣음
-        Vector3 objectPosition = new Vector3(530,128,0); 
-        // Debug.Log("mouse position : " + mousePosition); 
+        Vector3 objectPosition = new Vector3(438,78,0); 
+        Debug.Log("mouse position : " + mousePosition); 
         // Debug.Log("object position : " + objectPosition);
         float directionY = mousePosition.y - objectPosition.y;
         float directionX = mousePosition.x - objectPosition.x;
         // Debug.Log("X : " + directionX  + " Y : " + directionY);
 
         float rotateDegree =  Mathf.Atan2(directionY, directionX) * Mathf.Rad2Deg;
-
-		// Debug.Log("rotateDegree : "+ rotateDegree );
-		m_transform.rotation = Quaternion.Euler (0f,0f, rotateDegree -90);
+        if (rotateDegree < 1) return ; // 각도가 작으면 걍 움직이지마
+		Debug.Log("rotateDegree : "+ rotateDegree );
+        // m_transform.transform.LookAt(new Vector3(0,0,rotateDegree));
+		m_transform.rotation = Quaternion.Euler (0f,0f, rotateDegree-90);
 		// Debug.Log("Rocket Rotate  : " + m_transform.rotation);
     }
 }
