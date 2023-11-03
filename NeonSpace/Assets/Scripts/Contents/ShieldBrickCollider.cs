8de7d22e773939230bd3c8deea2f48aceb85b048
JiunShin1214 <jiunsin15@naver.com>
2023-11-03 10:44:34 +0900

    [Feat] ShieldBrickCollider추가

---
+++ NeonSpace/Assets/Scripts/Contents/ShieldBrickCollider.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBrickCollider : MonoBehaviour
+{
+    public Brick Shieldbrick_;
+    void Start()
+    {
+        Shieldbrick_ = transform.parent.parent.GetComponent<Brick>();
+    }
+
+    void OnCollisionEnter2D(Collision2D cd)
+    {
+        Shieldbrick_.SetCollider(cd.gameObject);
+    }
+
+}
