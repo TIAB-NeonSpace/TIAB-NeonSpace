45d4de1af11fbe86ecf09402b873a635c30e986d
praylee0226 <praylee917537@gmail.com>
2023-11-08 01:54:19 +0900

    [Update] Rockect Pivot Position

--- NeonSpace/Assets/Scripts/Manager/RocketRoateManager.cs
+++ NeonSpace/Assets/Scripts/Manager/RocketRoateManager.cs
@@ -26,18 +26,30 @@ public class RocketRoateManager : MonoBehaviour
         Vector3 mousePosition = UICamera.lastEventPosition ;
         // 원래는 m_transform.position으로 값 받아왔지만
         // 마우스가 탐지하는 좌표랑 해당 좌표의 괴리값이 있어서 그냥 고정값으로 넣음
-        Vector3 objectPosition = new Vector3(438,78,0); 
-        Debug.Log("mouse position : " + mousePosition); 
-        // Debug.Log("object position : " + objectPosition);
+        // Vector3 objectPosition = new Vector3(438,78,0); //1차 
+        // Vector3 objectPosition = new Vector3(623,106,0); //2차 왜 출발값이 바뀌는거지 
+        Vector3 objectPosition = new Vector3(365,78,0); //3차 왜 출발값이 바뀌는거지 
+        // 공의 출발위치 = oobjectPosition.y + 43;
+
         float directionY = mousePosition.y - objectPosition.y;
         float directionX = mousePosition.x - objectPosition.x;
-        // Debug.Log("X : " + directionX  + " Y : " + directionY);
+        
+        // if (mousePosition.y < 200) return; // 일정 방향이상으로 안돌리게하기  근데 이게 y값만으로 생각하는거라 그냥 각도로 보는게 편함
 
         float rotateDegree =  Mathf.Atan2(directionY, directionX) * Mathf.Rad2Deg;
         if (rotateDegree < 1) return ; // 각도가 작으면 걍 움직이지마
-		Debug.Log("rotateDegree : "+ rotateDegree );
+		
         // m_transform.transform.LookAt(new Vector3(0,0,rotateDegree));
+        // Debug.Log ("Euler : " + Quaternion.Euler (0f,0f, rotateDegree-90));
 		m_transform.rotation = Quaternion.Euler (0f,0f, rotateDegree-90);
-		// Debug.Log("Rocket Rotate  : " + m_transform.rotation);
+		
+
+        //Debug
+        // Debug.Log("mouse position : " + mousePosition); 
+        // Debug.Log("object position : " + objectPosition);
+        // Debug.Log("X : " + directionX  + " Y : " + directionY);
+        // Debug.Log("Rocket Rotate  : " + m_transform.rotation);
+
+        
     }
 }
